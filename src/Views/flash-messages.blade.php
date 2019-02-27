6bd351806d211ef5e5841f2b28775594687d8aaa
Rahman Younus <rahman@industrious.agency>
2019-02-27 11:57:15 +0000

    removed container around flash

--- src/Views/flash-messages.blade.php
+++ src/Views/flash-messages.blade.php
@@ -1,14 +1,12 @@
 @if (count($flash))
-    <div class="container">
-        <div class="flash-messages">
-            @foreach ($flash as $message)
-                <div class="alert alert-{{ $message['class'] }} alert-dismissible fade show" role="alert">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    {{ $message['message'] }}
-                </div>
-            @endforeach
-        </div>
+    <div class="flash-messages">
+        @foreach ($flash as $message)
+            <div class="alert alert-{{ $message['class'] }} alert-dismissible fade show" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                {{ $message['message'] }}
+            </div>
+        @endforeach
     </div>
 @endif
